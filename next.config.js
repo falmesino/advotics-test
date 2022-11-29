397a3ad8d8b6471b5d4dc33af72e16553cf94a06
Falmesino Abdul Hamid <falmesino@gmail.com>
2022-11-30 04:11:48 +0700

    feat: add scss, configs, packages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
+  poweredByHeader: false,
   swcMinify: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles', 'components/**/*')]
+  }
 }
 
 module.exports = nextConfig
